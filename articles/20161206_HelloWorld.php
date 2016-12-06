f490c4527062712c110caf31d5615cc301c18c5b
sylafrs <sylaf@hotmail.fr>
2016-12-07 00:52:03 +0100

    HelloWorld article spaced

--- articles/20161206_HelloWorld.php
+++ articles/20161206_HelloWorld.php
@@ -1,23 +1,37 @@
 <?php start_article("Création du site web", "2016/12/06", "site web", "20161206_HelloWorld.php"); ?>
 <?php start_article_paragraph(); ?>
+
 Bonjour, bonjour !
+
 <?php next_article_paragraph(); ?>
+
 Et voici le premier article !<br/>
 Tout juste après l'achat du domaine et l'activation de l'hébergement gratuit d'ovh (start100m), il me fallait absolument faire mon premier article, malgré l'absence manifeste de quelque décor que ce soit.
+
 <?php stop_article_paragraph(); ?>
 <?php show_article_image("20161206_screenshot_half.jpg", "20161206_screenshot.jpg"); ?>
 <?php start_article_paragraph(); ?>
+
 Il faut dire.. que je ne suis pas vraiment fan des CMS, et toutes ces choses.. quand il s'agit de faire un truc perso complètement bidouillable <em>rapidement</em> (bon.. du coup.. PHP, hein, même si le faire en C pourrait être marrant, je préfère avoir un truc vite-fait).<br/>
-J'ai toujours tout plein d'idées de trucs à commencer, mais j'en termine rarement beaucoup et j'ai pour projet d'ajouter au fil de l'eau ici mes différentes idées mais pas que.");
+J'ai toujours tout plein d'idées de trucs à commencer, mais j'en termine rarement beaucoup et j'ai pour projet d'ajouter au fil de l'eau ici mes différentes idées mais pas que.
+
 <?php next_article_paragraph(); ?>
+
 Je ne compte pas faire 'juste un site-CV', j'aimerais y ajouter des tutos complètement variés, des éléments intéractifs (côté client seulement, par contre), si je me met sérieusement sur un projet, je pourrais vous expliquer les différents éléments qui m'ont bloqué, les solutions que j'ai apporté, je pourrais vous partager du code pratique complètement indépendant que je peux créer pour des projets persos ou même pro (les parties que j'ai écrites tout seul).
+
 <?php next_article_paragraph(); ?>
+
 Enfin bref: je me sens inspiré pour faire de ce site, un truc plutôt intéressant.. en espérant que cette idée ne coule pas comme tant d'autres !!<br/>
 Je tiens à préciser que tout ce que je pourrai mettre sur le site est en WTFPL, les pages du site y compris : tout SAUF.. mention contraire (et oui, si je partages une image qui n'est pas de moi.. :-°)
+
 <?php next_article_paragraph(); ?>
+
 Comme vous vous en doutez, ma vie de programmeur ne fait pas que de commencer, et j'ai donc déjà pas mal de trucs à vous partager, à vous dire; mais du coup, pour éviter de rester dans le passé, je commencerai par vous parler de ce qui se passe actuellement en article et, de temps en temps, vous rajouterai un vieux truc.
+
 <?php next_article_paragraph(); ?>
+
 Puisque le CSS est du code en soit, à chaque mise-à-jour majeure du design du site, j'ajouterai un screen-shot dans un dossier pour avoir une progression du site :D<br/>
 Le code du site en lui même se trouve sur <a href="https://github.com/sylafrs/site">Github</a> parce que pourquoi pas. Bon.. par contre vous vous doutez que je planquerai tous les différents mots de passe s'il y a ^^
+
 <?php stop_article_paragraph(); ?>
 <?php stop_article(); ?>
