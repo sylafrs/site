337ae3ce6dafc06bed4aa7d6e23cfd4ad13f94d3
Sylvain Lafon <moi@sylafrs.xyz>
2016-12-07 17:15:53 +0100

    add screenshot from 07/12/2016

--- articles/20161207_FirstStyle.php
+++ articles/20161207_FirstStyle.php
@@ -4,5 +4,11 @@
 Et voici un premier style sobre pour le site. Ce style utilise <a href="http://getbootstrap.com/">Bootstrap</a> avec un thème customisé.<br/>
 Je ferai un screenshot quand il fera jour, il est une heure du matin !
 
+<?php next_article_paragraph(); ?>
+
+Edit de 17h15 : Voici un screenshot : 
+
 <?php stop_article_paragraph(); ?>
+<?php show_article_image("20161207_screenshot_half.jpg", "20161207_screenshot.jpg"); ?>
+
 <?php stop_article(); ?>
