e57ceb5004bdceea3df7f5dd75e2e536c4b6cd24
sylafrs <sylaf@hotmail.fr>
2016-12-07 11:09:13 +0100

    Update 20161207_FirstStyle.php
    fix date

--- articles/20161207_FirstStyle.php
+++ articles/20161207_FirstStyle.php
@@ -1,4 +1,4 @@
-<?php start_article("Création du premier style", "2016/12/06", "site web", "20161207_FirstStyle.php"); ?>
+<?php start_article("Création du premier style", "2016/12/07", "site web", "20161207_FirstStyle.php"); ?>
 <?php start_article_paragraph(); ?>
 
 Et voici un premier style sobre pour le site. Ce style utilise <a href="http://getbootstrap.com/">Bootstrap</a> avec un thème customisé.<br/>
